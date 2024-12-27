7294d0204c3c1c618e2a8df9c3254c0bb6452bad
Joyce Yang <joyce.yang@berkeley.edu>
2024-12-26 20:13:25 -0800

    set up dark mode persistence

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 var darkModeButton = document.getElementById("dark-mode-button");
 var lightModeButton = document.getElementById("light-mode-button");
 
+if(localStorage.getItem("currTheme") === "dark") {
+    darkmode();
+}
+
 function darkmode() {
-    var link = document.createElement("link");
-    link.type = "text/css";
-    link.rel = "stylesheet";
-    link.href = "css/darkmode.css";
-    link.id = "dark-mode-css";
-    document.getElementsByTagName("head")[0].appendChild(link);
+    const theme = "dark";
+    document.body.classList.toggle(theme);
+    localStorage.setItem("currTheme", theme);
 
     darkModeButton.style.display = "none";
     lightModeButton.style.display = "block";
 }
 
 function lightmode() {
-    var linkToRemove = document.getElementById("dark-mode-css");
-    const body = document.getElementsByTagName("body");
-    linkToRemove.parentNode.removeChild(linkToRemove);
+    const theme = "dark";
+    document.body.classList.toggle(theme);
+    localStorage.setItem("currTheme", "default");
 
     lightModeButton.style.display = "none";
     darkModeButton.style.display = "block";
