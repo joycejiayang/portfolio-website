2cac9140bda088b02d382d1aead0aff8eeeb044c
Joyce Yang <joyce.yang@berkeley.edu>
2025-03-02 21:47:09 -0800

    simplified color mode button

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const colorModeButton = document.getElementById("color-mode-button");
 
+const lightModeImg = document.createElement("img");
+lightModeImg.src = "assets/cyan-ball.png";
+lightModeImg.alt = "Click to switch to dark mode";
+
+const darkModeImg = document.createElement("img");
+darkModeImg.src = "assets/purple-ball.png";
+darkModeImg.alt = "Click to switch to light mode";
+
 function setUpColorModeButton() {
     colorModeButton.addEventListener("click", function() {
         if (document.body.classList.contains("dark")) {
@@ -14,19 +22,13 @@ function setUpColorModeButton() {
 
 function lightmode() {
     let currButtonImg = colorModeButton.children[0];
-    let newButtonImg = document.createElement("img");
-    newButtonImg.src = "assets/cyan-ball.png";
-    newButtonImg.alt = "Click to switch to dark mode";
-    colorModeButton.replaceChild(newButtonImg, currButtonImg);
+    colorModeButton.replaceChild(lightModeImg, currButtonImg);
     localStorage.setItem("currTheme", "default");
 }
 
 function darkmode() {
     let currButtonImg = colorModeButton.children[0];
-    let newButtonImg = document.createElement("img");
-    newButtonImg.src = "assets/purple-ball.png";
-    newButtonImg.alt = "Click to switch to light mode";
-    colorModeButton.replaceChild(newButtonImg, currButtonImg);
+    colorModeButton.replaceChild(darkModeImg, currButtonImg);
     localStorage.setItem("currTheme", "dark");
 }
 
